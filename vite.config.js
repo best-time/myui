1bd8f07defcfdfe2bcf1ed171b26d1474a5abd28
yinwy <349057996@qq.com>
2025-08-30 09:10:39 +0800

    feat: 依赖修改

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,7 @@ export default defineConfig ({
   plugins: [
     vue (),
     AutoImport ({
+      imports: ['vue', 'vue-router', 'pinia'],
       resolvers: [ElementPlusResolver ()],
     }),
     Components ({
